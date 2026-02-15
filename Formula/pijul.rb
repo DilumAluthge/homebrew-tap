e81b217cc42cefdeb2f4ab3f23fdca9efe11fa53
Dilum Aluthge <dilum@aluthge.com>
2026-02-15 00:22:46 -0500

    Update pijul.rb

--- Formula/pijul.rb
+++ Formula/pijul.rb
@@ -7,9 +7,9 @@
 class Pijul < Formula
   desc "Patch-based distributed version control system"
   homepage "https://pijul.org"
-  url "https://crates.io/api/v1/crates/pijul/#{version}/download"
   version "1.0.0-beta.11"
   sha256 "8a4fc27aa81ee061310d57fce2df9cc45f3149ddb00bdfab2b816beb0359b13d"
+  url "https://crates.io/api/v1/crates/pijul/#{version}/download"
   # revision 1
 
   bottle do
