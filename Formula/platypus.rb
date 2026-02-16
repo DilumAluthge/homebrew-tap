02628a1bf63dd92647fbec8dbd1c436e1612cd47
Dilum Aluthge <dilum@aluthge.com>
2026-02-15 19:12:12 -0500

    Fix Platypus, and add a regression test
    Closes #19.
    
    Signed-off-by: Dilum Aluthge <5619885+DilumAluthge@users.noreply.github.com>

--- Formula/platypus.rb
+++ Formula/platypus.rb
@@ -3,6 +3,11 @@
 # This formula is based on the last version before removal
 # Credit: Homebrew contributors
 # License: BSD
+#
+# Parts of this formula are also based on:
+# https://github.com/sveinbjornt/Platypus/blob/85196da49d3efe6e87f04b5963f732bcbb7d6c9b/wip/platypus.rb
+# Credit: Sveinbjorn Thordarson, and other Platypus contributors
+# License: BSD
 
 class Platypus < Formula
   desc "Create macOS applications from {Perl,Ruby,sh,Python} scripts"
@@ -20,20 +25,39 @@ class Platypus < Formula
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2474dc8002e41be5143cfbd2bc5e6b5be44a0ff9934a041a43f31e374d7ee8a1"
   end
 
+  depends_on "base64" => :build
   depends_on "make" => :build
   depends_on xcode: ["8.0", :build]
   depends_on :macos
 
   def install
+    # Much of this section is based on:
+    # https://github.com/sveinbjornt/Platypus/blob/85196da49d3efe6e87f04b5963f732bcbb7d6c9b/wip/platypus.rb
+    # Credit: Sveinbjorn Thordarson, and other Platypus contributors
+
+    # Fix hardcoded paths in Common.h to point to Homebrew's share directory
+    # This ensures the 'platypus' tool can find 'ScriptExec' and 'MainMenu.nib'
+    inreplace "Common.h" do |s|
+      s.gsub! "/usr/local/share/platypus", pkgshare
+    end
+
     # Build Platypus
     system "make", "build_unsigned"
 
-    # Install binary, and man page
+    # Some debugging
+    puts Dir.pwd
+    system "ls", "-la", "products"
+
+    # Install the executable
     bin.install "products/platypus_clt" => "platypus"
+
+    # Install the man page
     man1.install "CLT/man/platypus.1"
 
-    # Install sub-binary parts to share
+    # Install the helper app and resources to #{pkgshare} (share directory)
+    #
     # Without this, platypus is not runnable
+    # https://github.com/DilumAluthge/homebrew-tap/issues/18
     # https://github.com/Homebrew/homebrew-core/issues/18734
     cd "products/ScriptExec.app/Contents" do
       pkgshare.install "Resources/MainMenu.nib", "MacOS/ScriptExec"
@@ -52,5 +76,22 @@ class Platypus < Formula
   test do
     system bin/"platypus", "--version"
     system bin/"platypus", "--help"
+
+    # # Regression test for https://github.com/DilumAluthge/homebrew-tap/issues/18
+    # File.open("my_platypus_test_script.bash", "w") do |f|
+    #   f.write('#!/usr/bin/env bash\n')
+    #   f.write('\n')
+    #   f.write('echo "Hello, Platypus on macOS!"\n')
+    #   f.write('read -p "Press Enter to exit"\n')
+    # end
+    # puts ENV["HOMEBREW_TEMP"]
+    # system bin/"platypus",
+    #        "-a", "MyPlatypusTestApp",
+    #        "-o", "Text Window",
+    #        "./my_platypus_test_script.bash",
+    #        "./MyPlatypusTestApp.app"
+    # # Some debugging
+    # puts Dir.pwd
+    # system "ls", "-la"
   end
 end
