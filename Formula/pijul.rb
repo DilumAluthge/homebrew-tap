f5edbdb2e33e5ce5c01c7aea2178a59b14abb3d3
Dilum Aluthge <5619885+DilumAluthge@users.noreply.github.com>
2026-02-20 12:25:43 +0000

    pijul: add 1.0.0-beta.11_1 bottle.

--- Formula/pijul.rb
+++ Formula/pijul.rb
@@ -13,6 +13,14 @@ class Pijul < Formula
   license "GPL-2.0"
   revision 1
 
+  bottle do
+    root_url "https://github.com/DilumAluthge/homebrew-tap/releases/download/pijul-1.0.0-beta.11_1"
+    sha256 cellar: :any,                 arm64_tahoe:   "8dfcff75c013c0aa33ffd76c4e0af8c1bb3a3ae65c586d044ffd92d155da2c19"
+    sha256 cellar: :any,                 arm64_sequoia: "c5f2fa54f6c6c7c25257cda12535544b493872c0c818f93710c497324ba4740f"
+    sha256 cellar: :any,                 arm64_sonoma:  "177de583c3e884463ec67c1edaae49bcd69fa78ff929669c2303a9cd8e7e4093"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dacb3a32cab3fc0ea30eb03b22eb0c27e7dc1f96f8656448f8bedea5c00233c5"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "libsodium"
