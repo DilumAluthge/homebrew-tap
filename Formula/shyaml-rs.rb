3c0ca6aff2e0015dd28b7b5e162b014668464ec3
Dilum Aluthge <5619885+DilumAluthge@users.noreply.github.com>
2026-03-01 01:07:12 +0000

    shyaml-rs: add 0.3.2 bottle.

--- Formula/shyaml-rs.rb
+++ Formula/shyaml-rs.rb
@@ -8,6 +8,14 @@ class ShyamlRs < Formula
   license "MIT"
   # revision 1
 
+  bottle do
+    root_url "https://github.com/DilumAluthge/homebrew-tap/releases/download/shyaml-rs-0.3.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e3b08a4b157d074dbb75866837ef88aba48fd149c72dd28c3313a3c402caf1ad"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "01e56b84c5686fdf18aed80a99e9c4895c12be47f41cdf82a3dc69e2797af156"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a102f8b66fd299a845a6d694e673cd9b9020558e87e2d9748327af46c485aeda"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d9bfee4c25a49eea84bb4394da8d12888d9270f83c8beee5eefe2f48e7dc1738"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@3"
 
